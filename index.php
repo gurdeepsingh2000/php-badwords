57dfba630d940084c06c285685dd79f53d4e8998
Gurdeep Singh <gurdeepsingh2000329@gmail.com>
2021-08-25 15:16:21 +0200

    Done

---
+++ index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+<?php 
+
+$paragrafo = 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita blanditiis at alias culpa? Quibusdam quidem nesciunt, minima tempora omnis nam, itaque distinctio sint deserunt, suscipit illo deleniti fugit minus autem.';
+$lunghezza = strlen($paragrafo);
+
+
+
+echo "<p><b>{$paragrafo} <br> la lunghezza di caratteri è di {$lunghezza}</b></p>";
+$badWord = $_GET['name'];
+$BadWordPar = str_ireplace($badWord, '***', $paragrafo);
+echo "<p><b>{$BadWordPar} <br> la lunghezza di caratteri è di {$lunghezza}</b></p>";
+?>
+
+
+    
+</body>
+</html>
+
+
